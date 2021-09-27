cc54d38385a903c1e0b8a53f3d0781b08c692304
freakbyte <freakbyte@gmail.com>
2021-09-27 22:32:38 +0200

    Create OperatingSystem.cs

---
+++ ImTool/OperatingSystem.cs
@@ -0,0 +1,15 @@
+﻿namespace ImTool
+{
+    using System.Runtime.InteropServices;
+
+    public static class OperatingSystem
+    {
+        public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        public static bool IsMacOS => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        public static bool IsLinux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        public static bool Is64BitProcess => System.Environment.Is64BitProcess;
+
+        public static string GetRuntimeIdentifier => (IsWindows ? "win" : IsLinux ? "linux" : IsMacOS ? "osx" : "unknown") + "-" + (Is64BitProcess ? "x64" : "x86");
+
+    }
+}
