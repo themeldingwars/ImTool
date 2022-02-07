98b63e0322ae3f78a659bdefad5c5ae51084b6ff
freakbyte <freakbyte@gmail.com>
2022-02-07 22:16:00 +0100

    Fix: Race condition when adding/removing tabs

--- ImTool/Window.Tabs.cs
+++ ImTool/Window.Tabs.cs
@@ -9,70 +9,80 @@ namespace ImTool
     {
         private List<Tab> tabs = new();
         private Tab activeTab;
+        private object key = new();
 
         public void AddTab(Tab tab)
         {
-            if (!tabs.Contains(tab))
+            lock (key)
             {
-                tab.Load();
-                tabs.Add(tab);
+                if (!tabs.Contains(tab))
+                {
+                    tab.Load();
+                    tabs.Add(tab);
+                }
             }
         }
 
         public void RemoveTab(Tab tab)
         {
-            if (tabs.Contains(tab))
+            lock (key)
             {
-                tabs.Remove(tab);
-                tab.Unload();
+                if (tabs.Contains(tab))
+                {
+                    tabs.Remove(tab);
+                    tab.Unload();
+                }
             }
         }
 
         private void SubmitTabs()
         {
-            foreach (Tab tab in tabs)
+            lock (key)
             {
-                TabStyleOverrides(true);
-                if (!ImGui.BeginTabItem(tab.Name))
+                foreach (Tab tab in tabs)
                 {
-                    ImGui.DockSpace(tab.DockSpaceID, new Vector2(0,0), ImGuiDockNodeFlags.KeepAliveOnly);
-                    TabStyleOverrides(false);
-                    continue;
-                }
-                
-                if (activeTab != tab)
-                {
-                    activeTab = tab;
-                }
-                
-                bool hasMainMenuBar = false;
-                if (tab.IsMainMenuOverridden)
-                {
-                    hasMainMenuBar = true;
-                    BeginMainMenuBar();
-                    tab.InternalSubmitMainMenu();
-                    EndMainMenuBar();
-                }
-                else if (OnSubmitGlobalMenuBarOverride != null)
-                {
-                    hasMainMenuBar = true;
-                    BeginMainMenuBar();
-                    OnSubmitGlobalMenuBarOverride();
-                    EndMainMenuBar();
-                }
-                
-                TabStyleOverrides(false);
+                    TabStyleOverrides(true);
+                    if (!ImGui.BeginTabItem(tab.Name))
+                    {
+                        ImGui.DockSpace(tab.DockSpaceID, new Vector2(0, 0), ImGuiDockNodeFlags.KeepAliveOnly);
+                        TabStyleOverrides(false);
+                        continue;
+                    }
 
-                int mainMenuHeight = hasMainMenuBar ? (int) ImGui.GetItemRectSize().Y : 0;
-                Vector2 dockPos = new Vector2(borderThickness, borderThickness + titlebarHeight + mainMenuHeight);
-                Vector2 dockSize = contentBounds.Size - new Vector2(0, mainMenuHeight);
-                
-                tab.InternalSubmitDockSpace(dockPos, dockSize);
-                tab.InternalSubmitContent();
-                ImGui.EndTabItem();
-                
+                    if (activeTab != tab)
+                    {
+                        activeTab = tab;
+                    }
+
+                    bool hasMainMenuBar = false;
+                    if (tab.IsMainMenuOverridden)
+                    {
+                        hasMainMenuBar = true;
+                        BeginMainMenuBar();
+                        tab.InternalSubmitMainMenu();
+                        EndMainMenuBar();
+                    }
+                    else if (OnSubmitGlobalMenuBarOverride != null)
+                    {
+                        hasMainMenuBar = true;
+                        BeginMainMenuBar();
+                        OnSubmitGlobalMenuBarOverride();
+                        EndMainMenuBar();
+                    }
+
+                    TabStyleOverrides(false);
+
+                    int mainMenuHeight = hasMainMenuBar ? (int) ImGui.GetItemRectSize().Y : 0;
+                    Vector2 dockPos = new Vector2(borderThickness, borderThickness + titlebarHeight + mainMenuHeight);
+                    Vector2 dockSize = contentBounds.Size - new Vector2(0, mainMenuHeight);
+
+                    tab.InternalSubmitDockSpace(dockPos, dockSize);
+                    tab.InternalSubmitContent();
+                    ImGui.EndTabItem();
+
+                }
             }
-            
+
             TabStyleOverrides(false);
         }
     }
