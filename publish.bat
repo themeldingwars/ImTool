16adbf3820105a1ca37235a45f6281076f5b5faf
freakbyte <freakbyte@gmail.com>
2022-02-12 12:39:27 +0100

    Update to .NET 6
    - Bring both ImTool and Demo to .NET6
    - Update ImTool package version to 1.3.2
    - Updated publish.bat to enable compression of the demo binary
      (self-contained win-x64 binary is now 17.5mb! )

--- publish.bat
+++ publish.bat
@@ -1,3 +1,3 @@
 
-dotnet publish Demo/Demo.csproj -r win-x64 -c Release -o ./publish /p:PublishSingleFile=true /p:PublishTrimmed=true /p:TrimMode=Link /p:IncludeAllContentForSelfExtract=true --self-contained true
+dotnet publish Demo/Demo.csproj -r win-x64 -c Release -o ./publish /p:PublishSingleFile=true /p:EnableCompressionInSingleFile=true /p:PublishTrimmed=true /p:TrimMode=Link /p:PublishReadyToRun=false /p:IncludeAllContentForSelfExtract=true --self-contained true
 move publish\Demo.exe publish\Demo-win-x64.exe
