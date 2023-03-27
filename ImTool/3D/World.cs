b16c8d6ee37b1d1e1e178227eae115696898a774
Goomii <5684215+GoomiiV2@users.noreply.github.com>
2023-03-27 18:04:14 +0100

    Added world for the 3d renderer

---
+++ ImTool/3D/World.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veldrid;
+
+namespace ImTool.Scene3D
+{
+    public class World
+    {
+        public World()
+        {
+
+        }
+
+        public void Update(double dt)
+        {
+
+        }
+
+        public void Render(double dt, CommandList cmdList)
+        {
+
+        }
+    }
+}
--- ImTool/Widgets/Scene3dWidget.cs
+++ ImTool/Widgets/Scene3dWidget.cs
@@ -1,21 +1,50 @@
 ﻿
+using ImTool.Scene3D;
+
 namespace ImTool
 {
     public class Scene3dWidget : SceneWidget
     {
+        protected bool IsExternalWorld;
+        protected World WorldScene;
+
+        public bool ShowDebugInfo = true;
+
         // Create a scene to render a world and manage the worklld itself
         public Scene3dWidget(Window win) : base(win)
         {
+            IsExternalWorld = false;
+            WorldScene = new World();
+        }
+
+        // Crate a scene for an exteranly managed world, eg a camera view into one
+        public Scene3dWidget(Window win, World world) : base(win)
+        {
+            IsExternalWorld = true;
+            WorldScene = world;
         }
 
         public override void Render(double dt)
         {
             base.Render(dt);
+
+            if (!IsExternalWorld)
+                WorldScene.Update(dt);
+
+            WorldScene.Render(dt, CommandList);
         }
 
         public override void DrawOverlays()
         {
             base.DrawOverlays();
+
+            if (ShowDebugInfo)
+                DrawDebugOverlay();
+        }
+
+        private void DrawDebugOverlay()
+        {
+
         }
     }
 }
