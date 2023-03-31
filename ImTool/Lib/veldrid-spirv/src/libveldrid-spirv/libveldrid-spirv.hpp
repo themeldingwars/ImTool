01f11def042edc38b8ae59e7dfc116e4f3404813
Goomii <5684215+GoomiiV2@users.noreply.github.com>
2023-03-31 22:18:25 +0100

    :<

--- ImTool/Lib/veldrid-spirv/src/libveldrid-spirv/libveldrid-spirv.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef _WIN32
-#define VD_EXPORT extern "C" __declspec(dllexport)
-#else
-#define VD_EXPORT extern "C" __attribute__((visibility("default")))
-#endif
---
+++ ImTool/Shaders/SPIR-V/3D/Grid/GridVert.glsl
@@ -0,0 +1,70 @@
+﻿#version 450
+
+layout(set = 0, binding = 0) uniform ViewStateBuffer
+{
+    mat4 View;
+    mat4 Projection;
+    vec3 CamPos;
+    float padding1;
+    vec3 CamDir;
+    float padding2;
+    float CamNear;
+    float CamFar;
+} View;
+
+layout(set = 1, binding = 0) uniform WorldBuffer
+{
+    mat4 World;
+};
+
+layout(location = 0) out float near;
+layout(location = 1) out float far;
+layout(location = 2) out vec3 nearPoint;
+layout(location = 3) out vec3 farPoint;
+layout(location = 4) out mat4 fragView;
+layout(location = 8) out mat4 fragProj;
+
+// Grid position are in xy clipped space
+vec3 gridPlane[6] = vec3[](
+    vec3(1, 1, 0), vec3(-1, -1, 0), vec3(-1, 1, 0),
+    vec3(-1, -1, 0), vec3(1, 1, 0), vec3(1, -1, 0)
+);
+
+vec3 UnprojectPoint(float x, float y, float z, mat4 view, mat4 projection) {
+    mat4 viewInv = inverse(view);
+    mat4 projInv = inverse(projection);
+    vec4 unprojectedPoint =  viewInv * projInv * vec4(x, y, z, 1.0);
+    return unprojectedPoint.xyz / unprojectedPoint.w;
+}
+
+void main() {
+    vec3 p = gridPlane[gl_VertexIndex].xyz;
+    nearPoint = UnprojectPoint(p.x, p.y, 0.0, View.View, View.Projection).xyz; // unprojecting on the near plane
+    farPoint = UnprojectPoint(p.x, p.y, 1.0, View.View, View.Projection).xyz; // unprojecting on the far plane
+    gl_Position = vec4(p, 1.0); // using directly the clipped coordinates
+    
+    fragView = View.View;
+    fragProj = View.Projection;
+    
+    near = View.CamNear;
+    far = View.CamFar;
+}
+
+/*
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec2 TexCoords;
+layout(location = 0) out vec2 fsin_texCoords;
+layout(location = 1) out vec4 FragPos;
+
+void main()
+{
+    vec4 worldPosition = World * vec4(Position, 1);
+    vec4 viewPosition = View * worldPosition;
+    vec4 clipPosition = Projection * viewPosition;
+    //gl_Position = clipPosition;
+
+    gl_Position = Projection * View * World * vec4(Position, 1);
+    FragPos = worldPosition;
+
+    fsin_texCoords = TexCoords;
+}*/
