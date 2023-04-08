b7696b2fa9427a2948ba9e4ff527cde404c9cdea
Goomii <5684215+GoomiiV2@users.noreply.github.com>
2023-04-08 05:07:53 +0100

    simple mesh in :D

---
+++ ImTool/Shaders/SPIR-V/3D/Mesh/MeshFrag.glsl
@@ -0,0 +1,23 @@
+﻿#version 450
+
+layout(location = 0) in vec2 Uvs;
+layout(location = 1) in vec2 NormUvs;
+layout(location = 2) in vec3 Norm;
+layout(location = 3) in vec3 Tang;
+layout(location = 4) flat in int Color;
+layout(location = 5) in vec3 FragPos;
+
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    //outColor = vec4(1, 1, 1, 1);
+    
+    vec3 norm = normalize(Norm);
+    vec3 lightDir = normalize(vec3(0, 10, 0) - FragPos);
+    
+    float diff = max(dot(norm, lightDir), 0.0) + 0.2f;
+    vec3 diffuse = diff * vec3(0.5, 0.1, 0.7);
+
+    outColor = vec4(diffuse, 1);
+    outColor = vec4(1, 1, 1, 1);
+}
---
+++ ImTool/Shaders/SPIR-V/3D/Mesh/MeshVert.glsl
@@ -0,0 +1,47 @@
+﻿#version 450
+
+layout(set = 0, binding = 0) uniform ViewStateBuffer
+{
+    mat4 View;
+    mat4 Projection;
+    vec3 CamPos;
+    float padding1;
+    vec3 CamDir;
+    float padding2;
+    float CamNear;
+    float CamFar;
+} View;
+
+layout(set = 1, binding = 0) uniform WorldBuffer
+{
+    mat4 World;
+};
+
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec2 Uvs;
+layout(location = 2) in vec2 NormUvs;
+layout(location = 3) in vec3 Norm;
+layout(location = 4) in vec3 Tang;
+
+layout(location = 0) out vec2 out_Uvs;
+layout(location = 1) out vec2 out_NormUvs;
+layout(location = 2) out vec3 out_Norm;
+layout(location = 3) out vec3 out_Tang;
+layout(location = 4) out vec3 out_Color;
+layout(location = 5) out vec3 out_FragPos;
+
+void main() {
+    vec4 worldPosition = World * vec4(Position, 1);
+    vec4 viewPosition = View.View * worldPosition;
+    vec4 clipPosition = View.Projection * viewPosition;
+    
+    gl_Position = clipPosition;
+    out_Uvs = Uvs;
+    
+    out_FragPos = vec3(World * vec4(Position, 1.0));
+     
+    out_NormUvs = NormUvs;
+    out_Norm = mat3(transpose(inverse(World))) * Norm;
+    //out_Norm = Norm;
+    out_Tang = Tang;
+}
